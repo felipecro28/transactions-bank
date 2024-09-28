eb3204170a2efd485e131b2af27efe949427247c
felipecro28 <oliveira.felipe28@gmail.com>
2024-09-28 10:59:49 -0300

    feat: added prisma service to handle incoming data

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
 import { UserRepository } from "./user.repository";
+import { PrismaService } from "src/prisma.service";
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, UserRepository],
+  providers: [UserService, UserRepository, PrismaService],
 })
 export class UserModule {}
