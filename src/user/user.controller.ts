7607d96b9f0f6234e9cebb5eeb1269bf18d29002
felipecro28 <oliveira.felipe28@gmail.com>
2024-09-28 16:01:23 -0300

    feat: create user service and controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Response } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { CreateUserDTO } from "./dto/create-user.dto";
 
-@Controller('user')
-export class UserController {}
+@Controller("user")
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post("/")
+  async create(@Body() body: CreateUserDTO, @Response() res) {
+    const user = await this.userService.create(body);
+    return res.status(201).json(user);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { UserRepository } from "./user.repository";
+import { CreateUserDTO } from "./dto/create-user.dto";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async create(data: CreateUserDTO) {
+    try {
+      const user = await this.userRepository.create(data);
+      if (!user.id) {
+        throw new HttpException(
+          "Erro ao criar o usuário",
+          HttpStatus.BAD_REQUEST
+        );
+      }
+
+      return user;
+    } catch (error) {
+      if (error.message.includes("already exists")) {
+        throw new HttpException(
+          "Usuário já existente: Já existe um usuário cadastrado com o e-mail/documento informado.",
+          HttpStatus.CONFLICT
+        );
+      }
+      throw new HttpException(
+        `Ocorreu um erro: ${error.message}`,
+        error.status
+      );
+    }
+  }
+}
