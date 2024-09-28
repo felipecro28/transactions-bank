ee4c46b3cfdfaaa648e217b42fed8bf61efca2ce
felipecro28 <oliveira.felipe28@gmail.com>
2024-09-28 11:00:00 -0300

    feat: added create method on user repository

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,4 +1,16 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma.service";
+import { CreateUserDTO } from "./dto/create-user.dto";
 
 @Injectable()
-export class UserRepository {}
+export class UserRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(data: CreateUserDTO) {
+    const { document, email, full_name, kind, password } = data;
+    const user = await this.prismaService
+      .$executeRaw`INSERT INTO Users (document, email, full_name, kind, password) VALUES (${document}, ${email}, ${full_name}, ${kind}, ${password})`;
+
+    return user;
+  }
+}
