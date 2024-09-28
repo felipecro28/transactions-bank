ed6486d2f417ba5f77dbf343e731aa52ee4f9578
felipecro28 <oliveira.felipe28@gmail.com>
2024-09-28 11:04:49 -0300

    fix: table name and created findUnique method

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma.service";
 import { CreateUserDTO } from "./dto/create-user.dto";
+import { User } from "@prisma/client";
 
 @Injectable()
 export class UserRepository {
@@ -9,7 +10,14 @@ export class UserRepository {
   async create(data: CreateUserDTO) {
     const { document, email, full_name, kind, password } = data;
     const user = await this.prismaService
-      .$executeRaw`INSERT INTO Users (document, email, full_name, kind, password) VALUES (${document}, ${email}, ${full_name}, ${kind}, ${password})`;
+      .$executeRaw`INSERT INTO User (document, email, full_name, kind, password) VALUES (${document}, ${email}, ${full_name}, ${kind}, ${password})`;
+
+    return user;
+  }
+
+  async findUnique(id: number): Promise<User | null> {
+    const user: User | null = await this.prismaService
+      .$queryRaw`SELECT document, email, full_name, kind, password FROM User WHERE id = ${id}`;
 
     return user;
   }
