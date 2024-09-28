0c701ba4f9279af9cb34a3f5e06c76da54e7cb24
felipecro28 <oliveira.felipe28@gmail.com>
2024-09-28 10:49:29 -0300

    feat: created users dtos

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,41 @@
-export class CreateuserDto {}
+import {
+  IsAlphanumeric,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsNumberString,
+  IsString,
+  Length,
+  MinLength,
+} from "class-validator";
+
+export enum UserType {
+  Shopkeeper = "shopkeeper",
+  Regular = "regular",
+}
+
+export class CreateUserDTO {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  full_name: string;
+
+  @IsNumberString()
+  @IsNotEmpty()
+  @Length(14)
+  document: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsAlphanumeric()
+  @MinLength(4)
+  password: string;
+
+  @IsEnum(UserType)
+  @IsNotEmpty()
+  kind: UserType;
+}
